d94d3826c2f0fb091b6e56a3e285798000724bf9
karturbis <vulkanera42@gmx.ch>
2024-04-20 17:14:01 +0200

    change cryptography algorithm to ceasar

--- symmetric_encryption.py
+++ symmetric_encryption.py
@@ -1,31 +1,23 @@
-import os
-import base64
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import string
+ALPHABET = string.ascii_uppercase
 
-def int_to_fernet_key(key):
-    key = str(key).encode()
 
-    kdf = PBKDF2HMAC(
-        algorithm = hashes.SHA256(),
-        length = 32,
-        salt = b'',
-        iterations = 320000
-    )
-    aes_key = base64.b64encode(kdf.derive(key))
-    return Fernet(aes_key)
 
 def encrypt(message, key):
-    fernet_key = int_to_fernet_key(key)
-    print(f"DEBUGING: ENcrypt fernet key = {fernet_key}")
-    chiffre = fernet_key.encrypt(message.encode())
+    chiffre = ""
+    message = message.upper()
+    for i in message:
+        message_number = ALPHABET.index(i)
+        message_and_key = (message_number+key) % len(ALPHABET)
+        chiffre = chiffre + (ALPHABET[message_and_key])
     return chiffre
+    
 
 def decrypt(chiffre, key):
-    print(f"DEBUGING: symmetric_encryption.decrypt() chiffre = {chiffre}, key = {key}")
-    fernet_key = int_to_fernet_key(key)
-    message = fernet_key.decrypt(chiffre)
-    return message.decode()
-
-#print(decrypt(b'gAAAAABmI8az-IkZpmRY-_5tLZqI2pdCimmqiSS4b-CdRudy06c3cmEVJUIpp1Qf0oZbUvmWrQsdimdBMOToWa4fDTEopDtLFA==', 84027))
+    message = ""
+    chiffre = chiffre.upper()
+    for i in chiffre:
+        chiffre_number = ALPHABET.index(i)
+        chiffre_and_key = (chiffre_number-key) % len(ALPHABET)
+        message = message + (ALPHABET[chiffre_and_key])
+    return message
