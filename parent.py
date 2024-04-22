00bade1de3619892558b4e7663aca3dd1f65a0fa
karturbis <vulkanera42@gmx.ch>
2024-04-22 23:16:09 +0200

    clean up, write comments

--- parent.py
+++ parent.py
@@ -1,4 +1,10 @@
-"""This file contains parent classes."""
+"""This file contains the Endpoint class,which can be instatiated as
+a server or a client. The only differnce is, that the client starts
+the connection, after Key-exchange, both have same funktionality.
+For comunnication, a file writing system is used. Each instance
+of the Endpoint class has an input- and an output-file. If the
+Endpoint wants to send something, it writes it to the output-file."""
+
 import sys
 import random
 import hashlib
@@ -8,8 +14,6 @@ import symmetric_encryption
 
 class Endpoint:
     """This class contains all methods needed by an endpoint"""
-
-    
 
     def __init__(self, input_traffic_file, output_traffic_file, name, mode):
         self.input_traffic_file = input_traffic_file
@@ -21,7 +25,9 @@ class Endpoint:
         self.main()
 
     def gen_prime(self, a = 2**16, b = 2**17):
-        """Generates a random prime number between a and b"""
+        """Generates a random prime number between a and b,
+        by generating a list of primes and then pick a
+        random element of the list."""
         primes = []
         for i in range(a, b):
             if sympy.isprime(i):
@@ -33,62 +39,74 @@ class Endpoint:
         public_prime = 42, peer_pub_key = 42
         ):
         """Generates the hello message for
-        the key exchange."""
-        public_generator, public_prime, self_pub_key = self.pub_key_gen(public_generator, public_prime, peer_pub_key)
-
+        the key exchange.
+        See pub_key_gen() for more information, about all
+        parameters being 42 as the default."""
+        public_generator, public_prime, self_pub_key = self.pub_key_gen(
+            public_generator, public_prime, peer_pub_key
+            )
         return str(
             self.mode + "_HELLO::" + self.name + "::"
-            + str(public_generator) + "::" + str(public_prime) + "::" + str(self_pub_key)
+            + str(public_generator) + "::"
+            + str(public_prime) + "::" + str(self_pub_key)
             )
 
     def gen_message(self, cipher, message_hash):
-        """Generates a sendable message from a cipher and the message hash."""
+        """Generates a sendable message from
+        a cipher and the message hash."""
         return str("MESSAGE::" + str(cipher) + "::" + str(message_hash))
 
     def priv_key_gen(self, public_prime):
-        """This method creates the private key."""
+        """Creates the private key, which is a
+        random integer smaller than public_prime."""
         return random.randint(1, int(public_prime) - 1)
 
     def pub_key_gen(
         self, public_generator = 42,
         public_prime = 42, peer_pub_key = 42
         ):
-        """This method generates the public key of the endpoint,
-        if no parameters are given, it also creates the public
-        prime and the public generator."""
-
+        """Generates the public key of the endpoint, if no
+        parameters are given or all given parameters are 42, it
+        also creates the public prime and the public generator.
+        Public key is generated, using the standart
+        Diffie-Hellman-Merkle method."""
         if public_generator == public_prime == peer_pub_key == 42:
             public_prime = self.gen_prime()
             public_generator = random.randint(2**12, int(public_prime)-1)
         self.priv_key = self.priv_key_gen(int(public_prime))
-        self_pub_key = (int(public_generator)**self.priv_key)%int(public_prime)
+        self_pub_key = (
+            int(public_generator)**self.priv_key)%int(public_prime
+            )
         return public_generator, public_prime, self_pub_key
 
     def shared_key_gen(self, public_prime, peer_pub_key):
-        """Returns the shared key, takes the public prime and the peer pub
-        key as parameters."""
+        """Returns the shared key, takes the public
+        prime and the peer pub key as parameters."""
         return (peer_pub_key**self.priv_key)%public_prime
 
     def send(self, message):
-        """Sends the given message to the output_traffic."""
+        """Sends the given message to the output_traffic"""
         with open(self.output_traffic_file, "w", encoding="utf-8") as sender:
             sender.write(message)
 
-    def listen(self, key_word):
-        """This method listens on the input file, until a message
-        comes in, then it returns the message."""
+    def keyword_listen(self, key_word):
+        """Listens on the input file, until a
+        message with the given keyword comes
+        in, then it returns the message."""
         listening = True
         while listening:
-            with open(self.input_traffic_file, "r", encoding="utf-8") as listener:
+            with open(
+                self.input_traffic_file, "r", encoding="utf-8"
+                ) as listener:
                 input_packet = listener.read()
                 if input_packet.startswith(key_word):
                     return input_packet.split("::")
 
     def key_exchange_client(self):
-        """This method initiates a key exchange."""
+        """Initiates a key exchange."""
         self.send(self.gen_hello_message())
         print("LOG: CLIENT_HELLO message has been send.")
-        incoming_packet = self.listen("SERVER_HELLO")
+        incoming_packet = self.keyword_listen("SERVER_HELLO")
         print("LOG: SERVER_HELLO message has been received.")
         shared_key = self.shared_key_gen(
             int(incoming_packet[3]),
@@ -99,8 +117,9 @@ class Endpoint:
         print(f"DEBUGING: shared key = {shared_key}")
 
     def key_exchange_server(self):
-        """This method waits for a keychange to be initiated."""
-        incoming_packet = self.listen("CLIENT_HELLO")
+        """Waits for a keychange to be initiated 
+        by the other endpoint."""
+        incoming_packet = self.keyword_listen("CLIENT_HELLO")
         print("LOG: CLIENT_HELLO message has been received.")
         self.send(self.gen_hello_message(
             incoming_packet[2],
@@ -116,8 +135,8 @@ class Endpoint:
         print(f"DEBUGING: shared key = {shared_key}")
 
     def chiffre_send(self, message):
-        """The method to send and receive messages,
-        after an key exchange is established."""
+        """Send and receive messages, after
+        an key exchange is established."""
         message_hash = int(hashlib.sha256(message.encode()).hexdigest(), 16)
         print(f"LOG: The message hash is: {message_hash}")
         print(f"DEBUGING: The message is: {message}")
@@ -129,43 +148,53 @@ class Endpoint:
         self.clear_receiving()
 
     def chiffre_receive(self):
-        """This method listens for encrypted, incoming
+        """Listens for encrypted, incoming
         packages with type 'MESSAGE'. If such packet
-        arrives, it verifies the hash, after that prints
-        and returns the message."""
-        incoming_packet = self.listen("MESSAGE")
+        arrives, it verifies the hash, after that it
+        prints and returns the message."""
+        incoming_packet = self.keyword_listen("MESSAGE")
         print("LOG: MESSAGE has been received.")
-        print(f"DEBUGING: The chaiffre is: {incoming_packet[1]}")
-        message = symmetric_encryption.decrypt(incoming_packet[1], self.shared_key)
+        print(f"DEBUGING: The chiffre is: {incoming_packet[1]}")
+        message = symmetric_encryption.decrypt(
+            incoming_packet[1], self.shared_key
+            )
         print("LOG: Message has been succesfully decrypted.")
-        if int(hashlib.sha256(message.encode()).hexdigest(), 16) == int(incoming_packet[2]):
+        if int(hashlib.sha256(message.encode()).hexdigest(), 16) == int(
+                incoming_packet[2]
+                ):
             print("LOG: Message has been succesfully verified")
             print(f"LOG: The message is: {message}")
             return message
         print("WARNING: Message can not be verified.")
-        print(f"WARNING: Sended hash is: {int(incoming_packet[2])}")
-        print(f"WARNING: Calculated hash is: {int(hashlib.sha256(message.encode()).hexdigest(), 16)}")
-        print(f"LOG: The message is: {message}")
+        print(f"WARNING: Received hash is: {int(incoming_packet[2])}")
+        print(
+            f"WARNING: Calculated hash is: "
+            f"{int(hashlib.sha256(message.encode()).hexdigest(), 16)}")
+        print(f"LOG: The unverified received message is: {message}")
 
     def main(self):
-        """The main method, redistributes the work,
-        that is to do."""
+        """The main method, checks the mode,
+        the program runs in and redistributes
+        the work."""
         if self.mode == "SERVER":
             self.key_exchange_server()
             if self.chiffre_receive() == "!CLOSE!":
                 self.clear_transmitting()
                 sys.exit(0)
             self.main_loop()
-
-        else:
+        elif self.mode == "CLIENT":
             self.key_exchange_client()
             self.main_loop()
+        else:
+            print("WARNING: No available mode was selected.")
 
     def main_loop(self):
         """Loop, to continously send and receive messages."""
         done = False
         while not done:
-            input_message = input("Please enter a message, or a command; '?' for help: ")
+            input_message = input(
+                "Please enter a message, or a command; '?' for help: "
+                )
             if input_message.lower == "?":
                 print("type !close! to close the connection")
             elif input_message.upper in ("!CLOSE!", "!C!", "!QUIT!", "!Q!"):
